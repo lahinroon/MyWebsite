fd14a810beb556c0714e3aa5b92b877e00b1e36f
Nihal <nihalnoor007@gmail.com>
2021-06-06 02:34:00 +1000

    Added signup and login pages

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from flask import (Flask, flash, redirect, render_template, request,
                    Response, session, url_for)
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from os import path
+from os import error, path
 from werkzeug.datastructures import ContentRange
+from werkzeug.security import generate_password_hash, check_password_hash
 
 DB_NAME = "blog.db"
 
@@ -27,7 +28,12 @@ class Blogpost(db.Model):
     content = db.Column(db.Text)
     slug = db.Column(db.String(50))
 
-""" def login_required(fn):
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(100))
+
+def login_required(fn):
     @functools.wraps(fn)
     def inner(*args, **kwargs):
         if session.get('logged_in'):
@@ -35,22 +41,52 @@ class Blogpost(db.Model):
         return redirect(url_for('login', next=request.path))
     return inner
 
+@app.route('/blog/signup/', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+
+        if password != password2:
+            flash('Passwords don\'t match', category=error)
+        elif len(password) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            new_user = User(username=username, 
+                            password=generate_password_hash(password, method='sha256'))
+    
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect(url_for('login')) 
+
+    return render_template('/blog/signup.html')
+
 @app.route('/blog/login/', methods=['GET', 'POST'])
 def login():
     next_url = request.args.get('next') or request.form.get('next')
     if request.method == 'POST' and request.form.get('password'):
+        username = request.form.get('username')
         password = request.form.get('password')
-        # TODO: If using a one-way hash, you would also hash the user-submitted
-        # password and do the comparison on the hashed versions.
-        if password == app.config['ADMIN_PASSWORD']:
-            session['logged_in'] = True
-            session.permanent = True  # Use cookie to store session.
-            flash('You are now logged in.', 'success')
-            return redirect(next_url or url_for('blog'))
+
+        user = User.query.filter_by(username=username).first_or_404()
+
+        if user:
+            if check_password_hash(user.password, password):
+                session['logged_in'] = True
+                session.permanent = True  # Use cookie to store session.
+                flash('You are now logged in.', 'success')
+           
+                return redirect(url_for('blog'))
+            else:
+                flash('Incorrect password.', 'danger')
         else:
-            flash('Incorrect password.', 'danger')
+            flash('User does not exist', category=error)
+
     return render_template('/blog/login.html', next_url=next_url)
 
+"""
 @app.route('/blog/logout/', methods=['GET', 'POST'])
 def logout():
     if request.method == 'POST':
@@ -61,7 +97,13 @@ def logout():
 @app.route("/blog")
 def index():
     posts = Blogpost.query.all()
-    return render_template('/blog/blog.html', posts=posts)
+    return render_template('/blog/blog.html', posts=posts) 
+
+@app.route('/blog/drafts/')
+#@login_required
+def drafts():
+    posts = Blogpost.query.filter_by(published = False).all()
+    return render_template('/blog/blog.html', posts=posts, drafts=True)
 
 @app.route('/blog/create/', methods=['GET', 'POST'])
 #@login_required
@@ -88,16 +130,23 @@ def addpost():
 
     return redirect(url_for('blog')) 
 
+@app.route('/blog/delete/<slug>/')
+def delete(slug):
+    post_to_delete = Blogpost.query.filter_by(slug=slug).first_or_404()
+
+    try:
+        db.session.delete(post_to_delete)
+        db.session.commit()
+        return redirect(url_for('blog'))
+    except:
+        'There was an error deleting this post'
+
+    return render_template('/blog/blog.html')
+
 '''@app.route('/blog/post/<int:post_id>')
 def post(post_id):
     post = Blogpost.query.get(post_id)
     return render_template('/blog/detail.html', post=post)'''
-
-@app.route('/blog/drafts/')
-#@login_required
-def drafts():
-    query = Entry.drafts().order_by(Entry.timestamp.desc())
-    return object_list('/blog/blog.html', query, check_bounds=False)
 
 @app.route('/blog/<slug>/')
 def detail(slug):
@@ -112,18 +161,19 @@ def edit(slug):
     post = Blogpost.query.filter_by(slug=slug).first_or_404()
     
     if request.method == 'POST':
+
         post.title = request.form.get('title')
         post.content = request.form.get('content')
         published = request.form.get('published')
         slug = re.sub(r'[^\w]+', '-', post.title.lower()).strip('-')
         post.slug = slug
-        
+
         if published == 'y':
             published = True
         else:
-            published = False         
+            published = False
         post.published = published
-   
+
         db.session.commit()
 
     return render_template('/blog/edit.html', post=post)
